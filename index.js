b6b6f9833112619568d485b1701d48e701f15d46
Ryan Andrews <rcandrews44@gmail.com>
2021-03-29 01:40:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = "Ryan is the champion!"
